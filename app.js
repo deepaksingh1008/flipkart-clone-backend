29ea371670b599b8ec5ff05f75be3a4afc59a71b
deepaksingh <ds4850716@gmail.com>
2024-07-02 17:17:57 +0530

    adding proxy

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const errorMiddleware = require("./middlewares/error");
 const app = express();
 
 // config
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
